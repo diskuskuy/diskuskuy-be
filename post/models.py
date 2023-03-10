5c086f67d00346ab9c3a3af7a29c57f8253869c4
Sonia Rahmawati <soniarahma27@gmail.com>
2023-03-10 13:17:35 +0700

    edit field foreignkey

--- post/models.py
+++ post/models.py
@@ -4,28 +4,12 @@ from django.contrib.auth.models import User
 from forum.models import Thread
 from datetime import datetime
 
-# Create your models here.
-# class Post(models.Model):
-#     title = models.CharField(max_length=100)
-#     tag = models.CharField(max_length=100)
-#     content = HTMLField()
-#     date = models.DateTimeField(verbose_name="Deadline (MM/DD/YYYY)")
-#     likes = models.ManyToManyField(User, blank=True, related_name='likes')
-#     # user = 
-
-#     def __str__(self):
-#         return self.title
-
-#     def number_of_likes(self):
-#         return self.likes.count()
-
 class InitialPost(models.Model):
     title = models.CharField(max_length=100, default="Title-1")
     tag = models.CharField(max_length=100, default="Pemicu")
     content = HTMLField(default=" ")
-    date = models.DateTimeField(verbose_name="Deadline (MM/DD/YYYY)", default=datetime.now)
-    # post = models.OneToOneField(Post, on_delete=models.CASCADE)
-    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, default=1)
+    date = models.DateTimeField(auto_now=True, editable=False)
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, default=1, related_name="initial_post")
     # lecturer = 
     # likes = models.ManyToManyField(User, blank=True, related_name='likes')
     # user = 
@@ -40,9 +24,8 @@ class ReplyPost(models.Model):
     title = models.CharField(max_length=100, default="Title-1")
     tag = models.CharField(max_length=100, default="Pendapat")
     content = HTMLField(default=" ")
-    date = models.DateTimeField(verbose_name="Deadline (MM/DD/YYYY)", default=datetime.now)
-    # post = models.OneToOneField(Post, on_delete=models.CASCADE)
-    initial_post = models.ForeignKey(InitialPost, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True, editable=False)
+    initial_post = models.ForeignKey(InitialPost, on_delete=models.CASCADE, related_name="reply_post")
     # user = 
     # likes = models.ManyToManyField(User, blank=True, related_name='likes')
     # user = 
@@ -57,11 +40,8 @@ class NestedReplyPost(models.Model):
     title = models.CharField(max_length=100, default="Title-1")
     tag = models.CharField(max_length=100, default="Pendapat")
     content = HTMLField(default=" ")
-    date = models.DateTimeField(verbose_name="Deadline (MM/DD/YYYY)", default=datetime.now)
-    # post = models.OneToOneField(Post, on_delete=models.CASCADE)
-    reply_post = models.ForeignKey(ReplyPost, on_delete=models.CASCADE)
-    # user = 
-    
+    date = models.DateTimeField(auto_now=True, editable=False)
+    reply_post = models.ForeignKey(ReplyPost, on_delete=models.CASCADE, related_name="nested_reply_post")
     # likes = models.ManyToManyField(User, blank=True, related_name='likes')
     # user = 
 
@@ -71,4 +51,3 @@ class NestedReplyPost(models.Model):
     # def number_of_likes(self):
     #     return self.likes.count()
 
-# class Reaction(models.Model):
--- post/serializers.py
+++ post/serializers.py
@@ -9,13 +9,13 @@ class NestedReplyPostSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ReplyPostSerializer(serializers.ModelSerializer):
-    nested_reply_post = NestedReplyPostSerializer(read_only=True)
+    nested_reply_post = NestedReplyPostSerializer(read_only=True, many=True)
     class Meta:
         model = ReplyPost
         fields = '__all__'
 
 class InitialPostSerializer(serializers.ModelSerializer):
-    reply_post = ReplyPostSerializer(read_only=True)
+    reply_post = ReplyPostSerializer(read_only=True, many=True)
     class Meta:
         model = InitialPost
         fields = '__all__'
