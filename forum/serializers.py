05b8b5c97f27c8cf7233ccff21cac20ad3886188
Sonia Rahmawati <soniarahma27@gmail.com>
2023-03-17 14:08:43 +0700

    add upload() for referenceFile discussionGuide in Thread

--- forum/serializers.py
+++ forum/serializers.py
@@ -38,12 +38,13 @@ class DiscussionGuideResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = DiscussionGuide
         fields = ('id','deadline','description','mechanism_expectation', 'state')
+        read_only_fields = ['state']
 
 class ThreadRequestSerializer(serializers.ModelSerializer):
     initial_post = InitialPostThreadSerializer()
-    reference_file = ReferenceFileResponseSerializer(read_only=True, many=True)
+    reference_file = ReferenceFileResponseSerializer(many=True)
     summary = SummaryResponseSerializer(read_only=True)
-    discussion_guide = DiscussionGuideResponseSerializer(read_only=True)
+    discussion_guide = DiscussionGuideResponseSerializer()
     
     class Meta:
         model = Thread
@@ -65,14 +66,33 @@ class ThreadRequestSerializer(serializers.ModelSerializer):
         return thread
 
     def update(self, instance, validated_data):
-        initial_post_data = validated_data.pop('initial_post')
-        initial_post = instance.initial_post
+        if 'initial_post' in validated_data:
+            initial_post_data = validated_data.pop('initial_post')
+            initial_post = instance.initial_post
+            initial_post.tag = initial_post_data.get('tag', initial_post.tag)
+            initial_post.content = initial_post_data.get('content', initial_post.content)
+            initial_post.save()
+
+        if 'reference_file' in validated_data:
+            reference_file_data = validated_data.pop('reference_file')
+            reference_file = ReferenceFile.objects.filter(thread=instance)
+            for ref in reference_file:
+                for ref_data in reference_file_data:
+                    ref.title = ref_data.get('title', ref.title)
+                    ref.url = ref_data.get('url', ref.url)
+                ref.save()
+
+        if 'discussion_guide' in validated_data:
+            discussion_guide_data = validated_data.pop('discussion_guide')
+            discussion_guide = instance.discussion_guide
+            discussion_guide.deadline = discussion_guide_data.get('deadline', discussion_guide.deadline)
+            discussion_guide.description = discussion_guide_data.get('description', discussion_guide.description)
+            discussion_guide.mechanism_expectation = discussion_guide_data.get('mechanism_expectation', discussion_guide.mechanism_expectation)
+            discussion_guide.save()
+
         instance.title = validated_data.get('title', instance.title)
         instance.save()
 
-        initial_post.tag = initial_post_data.get('tag', initial_post.tag)
-        initial_post.content = initial_post_data.get('content', initial_post.content)
-        initial_post.save()
         return instance
 
 class ThreadResponseSerializer(serializers.ModelSerializer): #buat tampilan di week
@@ -91,4 +111,3 @@ class DiscussionAnalyticsSerializer(serializers.Serializer):
     total_participants = serializers.IntegerField()
     non_participants = serializers.IntegerField()
     tags = serializers.DictField()
-    
