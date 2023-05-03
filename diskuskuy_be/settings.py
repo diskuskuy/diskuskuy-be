c52fd665582c71aa6ea5d6f21424ab095afa5245
nadilatusifa <nadilatusifa83@gmail.com>
2023-05-03 12:06:49 +0700

    set up postgresql on gcp

--- diskuskuy_be/settings.py
+++ diskuskuy_be/settings.py
@@ -104,23 +104,23 @@ WSGI_APPLICATION = 'diskuskuy_be.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'eiyykemo',
-#         'USER': 'eiyykemo',
-#         'PASSWORD': 'zr5LhvBKDYiUesF9rLQoht28lM3Tu5Jc',
-#         'HOST': 'tiny.db.elephantsql.com',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_diksusi',
+        'USER': 'diskusi_user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
